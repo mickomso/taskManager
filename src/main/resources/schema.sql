1aff3f5ff7cab987dd5ba7fd2fabae856cbf2147
Mike <miguelcompanysoler@gmail.com>
2020-10-18 17:49:33 +0200

    Changed date types to TIMESTAMP

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS PUBLIC.Task (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     description VARCHAR(255) NOT NULL,
-    created_on DATE NOT NULL,
-    updated_on DATE,
+    created_on TIMESTAMP NOT NULL,
+    updated_on TIMESTAMP,
     completed boolean
 );
