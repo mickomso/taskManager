c2333a778f79d4cf828f8de4a9d9b14d1f2e8050
Mike <miguelcompanysoler@gmail.com>
2020-10-20 21:22:47 +0200

    Tests TaskController

--- src/test/java/com/company/taskManager/controller/TaskControllerTest.java
+++ src/test/java/com/company/taskManager/controller/TaskControllerTest.java
@@ -1,41 +1,83 @@
 package com.company.taskManager.controller;
 
-/*
-@RunWith(SpringRunner.class)
-@WebMvcTest(TaskController.class)
+import com.company.taskManager.domain.Task;
+import com.company.taskManager.repository.TaskRepository;
+import com.company.taskManager.service.api.TaskService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
- */
-public class TaskControllerTest {
-/*
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+class TaskControllerTest {
+
+    Logger log = LoggerFactory.getLogger(TaskControllerTest.class);
+
     @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
     private TaskService taskService;
 
+    @MockBean
+    private TaskRepository taskRepository;
+
     @Test
-    public void findAllTasks() {
-        System.out.println("Initializing TaskManagerApplicationTests.findAllTasks TEST...");
-        List<Task> tasks = taskService.findAll();
-        assertNotNull(tasks);
-        assertFalse(tasks.isEmpty());
-        System.out.println("TEST TaskManagerApplicationTests.findAllTasks finished OK!");
+    void whenFindingItemsItShouldReturnAllItems() throws Exception {
+        log.info("TEST TaskControllerTest.whenFindingItemsItShouldReturnAllItems()");
+        // given
+        Task task = new Task();
+        task.setId(1);
+        task.setTitle("Task 1");
+        task.setDescription("Description 1");
+        List<Task> tasks = Arrays.asList(task);
+        given(this.taskService.findAll()).willReturn(tasks);
+
+        // when + then
+        this.mockMvc.perform(get("/api/v1/tasks"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("[{'id': 1,'title': 'Task 1';'description': 'Description 1'}]"));
     }
 
     @Test
-    public void findTaskById() {
-        System.out.println("Initializing TaskManagerApplicationTests.findTaskById TEST...");
-        Optional<Task> user = taskService.findTaskById(1);
-        assertNotNull(user.get());
-        System.out.println("TEST TaskManagerApplicationTests.findAllTasks finished OK!");
+    void whenAddingItemItShouldReturnTheSavedItem() throws Exception {
+        log.info("TEST TaskControllerTest.whenAddingItemItShouldReturnTheSavedItem()");
+        //given
+        Task task = new Task();
+        task.setTitle("Task 1");
+        task.setDescription("Description 1");
+        given(this.taskService.createNewTask(task)).willReturn(task);
+
+        //when + then
+        String json = asJsonString(task);
+        this.mockMvc.perform(post("/api/v1/tasks")
+                .contentType(MediaType.APPLICATION_JSON).content(json))
+                .andExpect(status().isCreated());
     }
 
-    @Test
-    public void createTask() {
-        System.out.println("Initializing TaskManagerApplicationTests.createTask TEST...");
-        Task task = new Task("Test Title Task", "Test Description Task", new Date(System.currentTimeMillis()),false);
-        Task savedTask = taskService.saveOrUpdateTask(task);
-        Task newTask = taskService.findTaskById(savedTask.getId()).get();
-        assertEquals("Test Title Task", newTask.getTitle());
-        assertEquals("Test Description Task", newTask.getDescription());
-        System.out.println("TEST TaskManagerApplicationTests.findAllTasks finished OK!");
+    private String asJsonString(final Object obj) {
+        try {
+            final ObjectMapper mapper = new ObjectMapper();
+            final String jsonContent = mapper.writeValueAsString(obj);
+            return jsonContent;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
-    */
 }
